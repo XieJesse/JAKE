d96736f6981ae8842272391b2c9338d48d302d63
JesseStuy20 <jxie20@stuy.edu>
2023-02-04 17:33:09 -0500

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -114,7 +114,7 @@ def collection():
     data = db.get_ranked_posts()
     new_data = []
     for post in data:
-        new_data.append([post[0],post[1],post[2],get_user_pfp(post[0])])
+        new_data.append([post[0],post[1],post[2],db.get_user_pfp(post[0])])
     return render_template("collection.html", data=new_data)
 
 def getWords():
