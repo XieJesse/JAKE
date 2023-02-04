0f6e1d106230065df44d6eec467c391be46ad004
kcao20 <kcao2022@gmail.com>
2023-02-04 13:14:06 -0500

    add app routes and create their respective html templates

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,21 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/game")
+def game():
+    return render_template("game.html")
+
+@app.route("collection")
+def collection():
+    return render_template("collection.html")
 
 if __name__ == "__main__":
     app.debug = True
