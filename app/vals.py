cfce0a22755f6c24ac8873f9eac37971efb738e9
ezhengofficial <ezhengofficial@gmail.com>
2023-02-04 13:28:19 -0500

    added vals.py

---
+++ app/vals.py
@@ -0,0 +1,23 @@
+NOUNS = ["your eyes", "your body", "i", "me", "you", "your",
+ "your toes", "a beautiful sunset", "the moon", "your heart",
+  "cupid", "my girlfriend", "my boyfriend", "couple",
+   "my best friend", "my friend", "a rose", "my unending love",
+    "your hands", "your mother", "your face", "my guilty pleasure",
+     "without you", "your swagger", "your smile", "your touch",]
+
+ISAREPHRASE = ["absurd", "absolutely stunning", "glowing tonight",
+ "effortlessly beautiful", "fully appreciate", "simply glorious",
+  "my one and only", "just perfect", "a national treasure",
+   "have a good time", "gorgeous", "the best", "sweet like sugar"]
+
+VERBS = ["looks", "make(s) me feel like", "feels", "loves",
+ "in", "doesn’t compare to", "tastes like",
+  "make(s) my heart beat uncontrollably", "make(s) me feel woefully incomplete",
+   "shine bright like a diamond", "fall for you", "feel out of this world",
+    "whisper into my ear", "have a dazzling smile", "got charisma (W rizz)",
+     "smell(s) like flowers", "take(s) my breath away", "lights up the room",
+      "make my heart skip a beat"]
+
+ENDINGS = ["Mate", "honey", "my love", "young chap", "babe", "baby", "sugar", "sugarboo", "honey buns", "sweetie", "sweetiepie", "sugar cubes", "cupcake", "cheerio", "my little dumpling", "felicia"]
+
+GUARANTEED = ["and", "with", "for", "is", "and"]
