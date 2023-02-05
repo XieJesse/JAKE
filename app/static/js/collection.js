bbcabc26d4ed2c12aa9951014b0722866bba328d
JesseStuy20 <jxie20@stuy.edu>
2023-02-05 00:08:59 -0500

    Added collection copy

--- app/static/js/collection.js
+++ app/static/js/collection.js
@@ -2,13 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
     const collection = document.querySelector("#coll")
     const posts = collection.getElementsByClassName("card animated animatedFadeInUp fadeInUp")
     console.log(posts);
-    for (let i = 0; i < posts.length; i++) { 
+    for (let i = 0; i < posts.length; i++) {
         button = posts[i].getElementsByClassName("fa-heart")[0]
 
         button.addEventListener("click", function () {
             console.log("clicked")
             this.classList.toggle('fa-regular');
             this.classList.toggle('fa-solid');
+        }
+
+        )
+        copy = posts[i].getElementsByClassName("btn-primary")[0]
+
+        copy.addEventListener("click", function() {
+          console.log("copy")
+          navigator.clipboard.writeText(posts[i].getElementsByClassName("content")[0].innerHTML);
         })
         //console.log(posts[i]);
     }
